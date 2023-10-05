bb42821eadf9a5269ab4c5af9387f8209735d5f2
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2023-10-05 14:24:59 -0300

    Update CreateTables.sql

--- Scripts sql/CreateTables.sql
+++ Scripts sql/CreateTables.sql
@@ -1,6 +1,8 @@
 /* Todas as constraints
 Pessoa: cpfPKpessoa
 Telefone: cpfPKtel, CPFFKtel
+Quarto: PKNUMquarto
+Paciente: CPFPKpaciente, CPFFKpaciente, NRQUARTOFKpaciente
 */
 SET search_path TO hosp, public;
 
@@ -15,6 +17,19 @@ CREATE TABLE IF NOT EXISTS Telefone(
 	cpfPessoa varchar(11)  NOT NULL,
 	
 	CONSTRAINT cpfPKtel PRIMARY KEY(cpfPessoa),
-	CONSTRAINT CPFFKtel FOREIGN KEY(cpfPessoa) REFERENCES Pessoa(cpf) ON DELETE CASCADE ON UPDATE CASCADE
-		
+	CONSTRAINT CPFFKtel FOREIGN KEY(cpfPessoa) REFERENCES Pessoa(cpf) ON DELETE CASCADE ON UPDATE CASCADE	
+);
+CREATE TABLE IF NOT EXISTS Quarto(
+	NUM_QUARTO int NOT NULL /* OU VARCHAR?*/
+	
+	CONSTRAINT PKNUMquarto PRIMARY KEY(NUM_QUARTO)
+);
+CREATE TABLE IF NOT EXISTS Paciente(
+	cpfPaciente varchar(11) NOT NULL,
+	sorotipagem char(2) NOT NULL,
+	NrQuarto int,
+	
+	CONSTRAINT CPFPKpaciente PRIMARY KEY(cpfPaciente),
+	CONSTRAINT CPFFKpaciente FOREIGN KEY(cpfPaciente) REFERENCES Pessoa(cpf) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT NRQUARTOFKpaciente FOREIGN KEY(NRQuarto) REFERENCES Quarto(NUM_QUARTO) ON DELETE SET NULL ON UPDATE CASCADE
 );
