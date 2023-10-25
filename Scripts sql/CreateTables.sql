039e673f6ffaef6c7e719f3a13035310da9faa1c
Felipe256 <fcavalcante2013.fc@gmail.com>
2023-10-25 10:35:46 -0300

    Adicionando comorbidade, consulta e exame

--- Scripts sql/CreateTables.sql
+++ Scripts sql/CreateTables.sql
@@ -55,3 +55,37 @@ CREATE TABLE IF NOT EXISTS Especialidade(
 	
 	CONSTRAINT crmFKespec FOREIGN KEY(crm) REFERENCES Medico(crm) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS Comorbidade(
+	cpf varchar(11) NOT NULL,
+	descricao varchar(100) NOT NULL,
+
+	CONSTRAINT cpfPKComorbidade PRIMARY KEY(cpf),
+	CONSTRAINT cpfFKComorbidade FOREIGN KEY(cpf) REFERENCES paciente(cpfpaciente) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Consulta(
+	id varchar(6) NOT NULL,
+	crm varchar(13) NOT NULL,
+	cpf varchar(11) NOT NULL,
+	horario timestamp NOT NULL,
+	status varchar(100) NOT NULL,
+
+	CONSTRAINT idPKConsulta PRIMARY KEY(id),
+	CONSTRAINT crmFKConsulta FOREIGN KEY(crm) REFERENCES Medico(crm) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT cpfFKConsulta FOREIGN KEY(cpf) REFERENCES Paciente(cpfpaciente) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Exame(
+	id varchar(6) NOT NULL,
+	crm varchar(13) NOT NULL,
+	cpf varchar(11) NOT NULL,
+	coren varchar(19) NOT NULL,
+	horario timestamp NOT NULL,
+	nome varchar(50) NOT NULL,
+
+	CONSTRAINT idPKExame PRIMARY KEY(id),
+	CONSTRAINT crmFKExame FOREIGN KEY(crm) REFERENCES Medico(crm) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT cpfFKExame FOREIGN KEY(cpf) REFERENCES Paciente(cpfpaciente) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT corenFKExame FOREIGN KEY(coren) REFERENCES Enfermeiro(coren) ON DELETE CASCADE ON UPDATE CASCADE
+);
